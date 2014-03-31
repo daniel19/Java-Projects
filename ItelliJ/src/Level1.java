1456c4f1089401e1f021a481b92ddbc7f51dcb58
MacDeveloper <daniel19brown@gmail.com>
2014-03-31 15:09:39 -0500

    Color themes

--- ItelliJ/src/Level1.java
+++ ItelliJ/src/Level1.java
@@ -3,7 +3,9 @@
  */
 public class Level1 {
 
-    public static void main(String[]    args){
+    public static void main(String[] args){
+
+
         System.out.print("Running Intelli J project");
     }
 }
