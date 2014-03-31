a5309764cbbbf33f73de1e13f082b3cf1e5b31cc
Daniel Brown <DanielBrown@DanielBrown-PC.(none)>
2014-03-31 13:38:59 -0500

    Java Projects

---
+++ ItelliJ/src/Level1.java
@@ -0,0 +1,9 @@
+/**
+ * Created by DanielBrown on 3/31/2014.
+ */
+public class Level1 {
+
+    public static void main(String[]    args){
+        System.out.print("Running Itelli J project");
+    }
+}
