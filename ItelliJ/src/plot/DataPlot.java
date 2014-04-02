8b57d854cd9ba33a42c3bc2bf9c3c854075ac30c
MacDeveloper <daniel19brown@gmail.com>
2014-04-02 15:44:07 -0500

    Adding lab 1

---
+++ ItelliJ/src/plot/DataPlot.java
@@ -0,0 +1,18 @@
+package  plot;
+
+import java.awt.*;
+import javax.swing.JPanel;
+
+public class DataPlot extends CenterFrame
+{
+
+   public DataPlot(int width, int height, String title, JPanel draw)
+   {
+      super(640, 480, title);
+
+      add(draw);  
+      setVisible(true);
+   }
+
+}
+
