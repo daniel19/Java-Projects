76f84f9ce2a93d36173b1635ffa5d698ffce7f41
MacDeveloper <daniel19brown@gmail.com>
2014-10-27 20:07:50 -0500

    Adding Javadoc for lab9

--- CSC 2120/Labs/Lab9/src/BinarySearchTree.java
+++ CSC 2120/Labs/Lab9/src/BinarySearchTree.java
@@ -1,12 +1,22 @@
+/**
+ * BinarySearchTree class creates binary search trees that abides
+ *  the binary tree property.
+ *  @author Daniel Brown
+ */
 public class BinarySearchTree extends BinaryTreeBasis implements SearchTreeInterface
 { 
-   private int size;
-
+    /**
+     * Size variable holds total number of nodes in BST.
+     */
+    private int size;
+   
+   /**
+    * BST Constructor that calls parent constructor BinaryTreeBasis
+    */ 
    public BinarySearchTree()
    {
       super();
    }
-
    public KeyedItem retrieve(Comparable searchKey) 
    {
        return retrieveItem(getRootNode(), searchKey);
@@ -26,6 +36,11 @@ public class BinarySearchTree extends BinaryTreeBasis implements SearchTreeInter
       assert validateBSTProperty() : "Not valid BST after " + searchKey + " deleted."; 
    }  
 
+   /**
+    * Called by retrieve() to navigate the BST.
+    * Preconditions: TreeNode and Comparable key to navigate the BST.
+    * Postconditions: KeyedItem returned that matches the search key.
+    */
    protected KeyedItem retrieveItem(TreeNode tNode, Comparable searchKey)
    {
       //disallow duplicates so that you always know which object to retrieve (or delete)
@@ -60,10 +75,13 @@ public class BinarySearchTree extends BinaryTreeBasis implements SearchTreeInter
       //assert treeItem != null || !treeItem.getKey().equals(searchKey) : searchKey + " Not found."; 
       return treeItem;
    }  
-
+    /**
+     *Called by insert() to place KeyedItem into BST.
+     *Preconditions: TreeNode and KeyedItem to be placed into BST.
+     *Postconditions: Treenode Inserted into BST.
+     */ 
    protected TreeNode insertItem(TreeNode tNode, KeyedItem item) throws TreeException
    {
-
       if (tNode == null) 
       {
          // position of insertion found; insert after leaf
@@ -93,10 +111,19 @@ public class BinarySearchTree extends BinaryTreeBasis implements SearchTreeInter
 
       return tNode;
    }  
+    /**
+     * Called by insertItem(); method decomp.
+     * Preconditions: Duplicate item found.
+     * Postconditions: Thrown TreeException
+     */
    protected void insertDuplicate() throws TreeException{
       throw new TreeException ("Cannot add duplicate.");
    }
-
+    /**
+     * Called by insertItem(); method decomp.
+     * Preconditions: Takes a TreeNode and KeyedItem to be inserted on the left size of a node.
+     * Postconditions: inserted node.
+     */
    protected TreeNode insertLeft(TreeNode t, KeyedItem item){
          // search the left subtree
          TreeNode subtree = insertItem(t.getLeft(), item);
@@ -104,6 +131,11 @@ public class BinarySearchTree extends BinaryTreeBasis implements SearchTreeInter
          t.setLeft(subtree);
          return t;
    }
+   /**
+     * Called by insertItem(); method decomp.
+     * Preconditions: Takes a TreeNode and KeyedItem to be inserted on the right size of a node.
+     * Postconditions: inserted node.
+     */
    protected TreeNode insertRight(TreeNode t, KeyedItem item){
          // search the right subtree
          TreeNode subtree = insertItem(t.getRight(), item);
@@ -111,9 +143,13 @@ public class BinarySearchTree extends BinaryTreeBasis implements SearchTreeInter
          t.setRight(subtree);
          return t; 
    }
+   /**
+     * Called by delete().
+     * Preconditions: Takes a TreeNode and KeyedItem to be deleted.
+     * Postconditions: Deleted node.
+     */
    protected TreeNode deleteItem(TreeNode tNode, Comparable searchKey) throws TreeException
    {
-
       if (tNode == null) 
       {
          throw new TreeException("Item not found");
@@ -132,19 +168,43 @@ public class BinarySearchTree extends BinaryTreeBasis implements SearchTreeInter
       else if (comparison < 0) 
       {
          // search the left subtree
-         subtree = deleteItem(tNode.getLeft(), searchKey);
-         tNode.setLeft(subtree);
+         tNode = deleteLeft(tNode, searchKey);
       }
       else 
       { 
          // search the right subtree
-         subtree = deleteItem(tNode.getRight(), searchKey);
-         tNode.setRight(subtree);
+         tNode = deleteRight(tNode, searchKey);
       } 
 
       return tNode;
    }  
-
+   /**
+     * Called by deleteItem(); method decomp.
+     * Preconditions: Takes a TreeNode and KeyedItem to be deleted.
+     * Postconditions: Deleted node.
+     */
+   protected TreeNode deleteLeft(TreeNode tNode, Comparable sk)
+   {
+      TreeNode subtree = deleteItem(tNode.getLeft(), sk);
+      tNode.setLeft(subtree);
+      return tNode;
+   }
+    /**
+     * Called by deleteItem(); method decomp.
+     * Preconditions: Takes a TreeNode and KeyedItem to be deleted.
+     * Postconditions: Deleted node.
+     */
+   protected TreeNode deleteRight(TreeNode tNode, Comparable sk)
+   {
+      TreeNode subtree = deleteItem(tNode.getRight(), sk);
+      tNode.setRight(subtree);
+      return tNode;
+   }
+   /**
+     * Called by deleteItem(); method decomp.
+     * Preconditions: Takes a TreeNode and KeyedItem to be deleted.
+     * Postconditions: Deleted node.
+     */ 
    protected TreeNode deleteNode(TreeNode tNode) 
    {
       // Algorithm note: There are four cases to consider:
@@ -183,7 +243,11 @@ public class BinarySearchTree extends BinaryTreeBasis implements SearchTreeInter
          return tNode;
       } 
    }  
-
+    /**
+     *Finds the left most leaf in a BST.
+     *Preconditions: TreeNode 
+     *Postconditions: KeyedItem
+     */
    protected KeyedItem findLeftmost(TreeNode tNode)  
    {
       if (tNode.getLeft() == null) 
@@ -195,7 +259,11 @@ public class BinarySearchTree extends BinaryTreeBasis implements SearchTreeInter
          return findLeftmost(tNode.getLeft());
       }  
    } 
-
+    /**
+     *Finds the left most leaf in a BST and deletes it.
+     *Preconditions: TreeNode 
+     *Postconditions: Deleted TreeNode
+     */
    protected TreeNode deleteLeftmost(TreeNode tNode) 
    {
       if (tNode.getLeft() == null) 
@@ -209,7 +277,11 @@ public class BinarySearchTree extends BinaryTreeBasis implements SearchTreeInter
          return tNode;
       }  
    } 
-
+    /**
+     *Rotates the specified TreeNode to the left.
+     *Preconditions: TreeNode 
+     *Postconditions: New BST based off of rotation properties.
+     */
    protected TreeNode rotateLeft(TreeNode tNode)
    {
       TreeNode right = tNode.getRight();
@@ -220,7 +292,11 @@ public class BinarySearchTree extends BinaryTreeBasis implements SearchTreeInter
 
       return right;
    }
-
+    /**
+     *Rotates the specified TreeNode to the right.
+     *Preconditions: TreeNode 
+     *Postconditions: New BST based off of rotation properties.
+     */
    protected TreeNode rotateRight(TreeNode tNode)
    {
       TreeNode left = tNode.getLeft();
@@ -231,15 +307,28 @@ public class BinarySearchTree extends BinaryTreeBasis implements SearchTreeInter
 
       return left;
    }
-
+    /**
+     * Returns the height of the BST.
+     * Preconditions: None
+     * Postconditions: integer height of BST.
+     */
    public int height(){
         return getHeight(getRootNode());
    }
+   /**
+    * Helper method to return height of BST.
+    * Preconditions: TreeNode to keep postion
+    * Postconditions: integer of BST height.
+    */
    protected int getHeight(TreeNode t){
         if(t == null)return 0;
         return 1 + Math.max(getHeight(t.getLeft()), getHeight(t.getRight()));
    }
-
+    /**
+     *Determines if BST is valid according to properties.
+     *Preconditions: none
+     *Postconditions: Boolean return of valid BST Property.
+     */
    public boolean validateBSTProperty(){
         TreeIterator it = iterator();
         it.setInorder();
@@ -260,7 +349,13 @@ public class BinarySearchTree extends BinaryTreeBasis implements SearchTreeInter
         }
         return valid;
    }
+
    public int size(){return size;}
+   /**
+    * Helper method to determine the size of the BST.
+    * Preconditions none
+    * Postconditions integer with size of BST.
+    */
    protected int computeSize(){
        TreeIterator it = iterator();
        it.setInorder();
@@ -269,12 +364,25 @@ public class BinarySearchTree extends BinaryTreeBasis implements SearchTreeInter
        return i;
 
    }
+   /**
+    * Determines if the instance variable matches the actual size of the BST.
+    * Preconditions: none
+    * Postconditions: boolean of the tested condition.
+    */ 
    public boolean validateSize(){ return size() == computeSize(); }
-
+    /**
+     * Determines if the BST is balanced according to BST property.
+     * Preconditions: none;
+     * Postconditions: Boolean of condition
+     */
    public boolean isBalanced(){
        return isBalanced(getRootNode()); 
    }
-
+    /**
+     * Helper method to determine if the BST is balanced according to BST property.
+     * Preconditions: TreeNode position.
+     * Postconditions: Boolean of condition
+     */
    protected boolean isBalanced(TreeNode tNode){
           if (tNode == null)
           {
