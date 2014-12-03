94d2111b2b90b45c24c103001bd207c92b14f978
MacDeveloper <daniel19brown@gmail.com>
2014-12-03 16:29:25 -0600

    Adding buttons and panels

---
+++ CSC 2120/Assignments/Program4/src/PartyGUIDriver.java
@@ -0,0 +1,7 @@
+import gui.PartyGUI;
+public class PartyGUIDriver {
+    public static void main(String[] args){
+        PartyGUI gui = new PartyGUI();
+        gui.setVisible(true);
+    }
+}
--- CSC 2120/Assignments/Program4/src/gui/PartyGUI.java
+++ CSC 2120/Assignments/Program4/src/gui/PartyGUI.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import party.PartyPlanner;
 
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * PartyGUI - class to hold all physical components.
@@ -23,9 +25,49 @@ public class PartyGUI extends JFrame{
 
     public PartyGUI(){
         super("Party Gui");
-        this.setLayout(new GridLayout(2,1));
+        this.setLayout(new FlowLayout());
         infoArea = new JTextArea();
         scrollPane = new JScrollPane(infoArea);
-        this.add(scrollPane,0);
+        this.add(scrollPane);
+        initButtonPanel();
+        this.add(panelButtons);
+        this.setSize(400, 480);
+            this.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    super.windowClosing(e);
+                    System.exit(0);
+                }
+            });
+    }
+
+    private void initButtonPanel(){
+        panelButtons = new JPanel();
+        panelButtons.setLayout(new FlowLayout());
+        buttonAccept = new JButton("Accept");
+        buttonSort = new JButton("Sort");
+        buttonADD = new JButton("ADD");
+        buttonStatus = new JButton("Status");
+        buttonRegret = new JButton("Regret");
+        buttonInvite = new JButton("Invite");
+        buttonHelp = new JButton("Help");
+        buttonPay = new JButton("Pay");
+        buttonPrices = new JButton("Prices");
+        buttonWho = new JButton("Who");
+        buttonGuests = new JButton("Guests");
+        buttonFile = new JButton("File");
+        panelButtons.add(buttonAccept);
+        panelButtons.add(buttonADD);
+        panelButtons.add(buttonFile);
+        panelButtons.add(buttonGuests);
+        panelButtons.add(buttonHelp);
+        panelButtons.add(buttonInvite);
+        panelButtons.add(buttonPay);
+        panelButtons.add(buttonADD);
+        panelButtons.add(buttonPrices);
+        panelButtons.add(buttonRegret);
+        panelButtons.add(buttonSort);
+        panelButtons.add(buttonStatus);
+        panelButtons.add(buttonWho);
     }
 }
